a479c6188a916bb2ed98ab8f4ee85ad05acb3f65
Michael Iyer <your-email@example.com>
2025-04-15 20:28:57 -0400

    Fixed up Ashen, etc

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ function updateFooter() {
   YOU ARE SLATED FOR HELL, PIGS!`;
 
   // Update countdown to midnight
-  document.getElementById("countdownToMidnight").textContent = `Countdown to Midnight: 🧌 ${getTimeUntilMidnightEastern()} Clocks: Thou Can't Tick Fast Enough! With no due respect, you make me want to die, GOP.`;
+  document.getElementById("countdownToMidnight").textContent = `Countdown to Midnight: 🧌 ${getTimeUntilMidnightEastern()} Clocks: Thou Clockest Can't Tick Fast Enough! With no due respect, you make me want to die, GOP.`;
 }
 
 // Start the clock
