acd23ec243cb088d52861142e5c56baded426840
Michael Iyer <your-email@example.com>
2025-04-22 06:10:03 -0400

    Spate in and out

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ function updateFooter() {
   YOU ARE SLATED FOR HELL, PIGS!`;
 
   // Update countdown to midnight
-  document.getElementById("countdownToMidnight").textContent = `Countdown to Midnight: 🧌 ${getTimeUntilMidnightEastern()} Clocks: Thou Clockest Can't Tick Fast Enough! With no due respect, you make me want to die, GOP.`;
+  document.getElementById("countdownToMidnight").textContent = `Countdown to Midnight: 🧌 ${getTimeUntilMidnightEastern()} Clocks: Thou Clockest Can't Tick Fast Enough! With no due respect, you make me want to die, GOP!`;
 }
 
 // Start the clock
@@ -291,7 +291,7 @@ function updateCountdown() {
   const countdownElement = document.getElementById("countdown");
   if (countdownElement) {
     countdownElement.textContent = `The American Mid-Term Elections 0f 2026: ${calculateCountdown("2026-11-03T00:00:00")}. 
-    Will We Goddamn MotherFucking Make Her Till Then, Thoun Jerkoes? Thou Dastardly Fools? Likely We Art Fucketh!`;
+    Will We Goddamn MotherFucking Make Her Till Then, Thou Jerkoes? Thou Dastardly Fools? Likely We Art Fucketh!!`;
   }
 }
 
