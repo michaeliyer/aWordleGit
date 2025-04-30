09024ac76caa2caa31474d81d3ea934d8aaa4b84
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-04-30 14:36:17 -0400

    Idler, Ols Americanos

--- clockStuff.js
+++ clockStuff.js
@@ -70,7 +70,7 @@ function calculateCountdown(targetDate) {
 function updateCountdown() {
   const countdownElement = document.getElementById("countdown");
   if (countdownElement) {
-    countdownElement.textContent = `American Mid-Term Elections 0f 2026: ${calculateCountdown("2026-11-03T00:00:00")}. 
+    countdownElement.textContent = `Los Americanos Mid-Term Elections 0f 2026: ${calculateCountdown("2026-11-03T00:00:00")}. 
     Will We Goddamn MotherFucking Make Her Till Then, Thou Jerkoes? Thou Dastardly Fools? Likely We Art Fucketh? Sayeth Thou?`;
   }
 }
