70a21c33e643fbdd784b87b9caf6b6f1fe45aeed
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-05-02 15:54:53 -0400

    Whose message

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ function applyFilter(selectedFunction, letter, position) {
       filteredWords = containsDuplicateLetters();
       break;
     default:
-      alert("Please select a valid function, you stupid asshole.");
+      alert("Please select a valid function, you stupid, stupid asshole.");
       return;
   }
 }
