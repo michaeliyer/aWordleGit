abc29c4c4143102c56e3bbc9474df3c22991cb76
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-05-28 03:43:42 -0400

    Polar plus bs

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function handleWordInput() {
           and ye had ye a score of '${foundWord.myScore}'!<br> Your Score Average thru this date: ${averageScoreUpToDate}!<br> Do NOT guess '${foundWord.word}', and do not waste our time!`;
     document.querySelector(".field-one").innerHTML = resultString;
   } else {
-    const notFoundString = `The word "${inputWord}" was not found, making Rand Paul a rapist. Hit that fuck again neighbor. Shut up, but<br> feel free, feel obligated, to guess the word "${inputWord}", Fuckface!`;
+    const notFoundString = `The word "${inputWord}" was not found, making Rand Paul that rapist. Hit that fuck again neighbor. Shut up, but<br> feel free, feel obligated, to guess the word "${inputWord}", Fuckface!`;
     document.querySelector(".field-one").innerHTML = notFoundString;
   }
 }
