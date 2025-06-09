56cc798be11908b776d72a2b561ae6bb4d044e31
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-06-08 23:34:54 -0400

    Lease plus bs

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function displayResults(results) {
   const resultsDiv = document.getElementById("filteredWords");
   const wordsList = results.join(", ");
   const totalWords = results.length;
-  const listWithTotal = `${wordsList} (${totalWords} Christing idiotic stupidAssHoleWords existParasitically in this here unlit motherfrickin' list, assholes!! It's steadily shrinking, dickLicks, like my sanity, DICKLORD!)`;
+  const listWithTotal = `${wordsList} (${totalWords} Christing idiotic stupid AssHole Words exist Parasitically in this here unlit motherfrickin' list, assholes!! It's steadily shrinking, dickLicks, like my sanity, DICKLORD!)`;
   resultsDiv.innerHTML = listWithTotal;
 }
 
