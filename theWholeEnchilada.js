6fcf2cce49515435a2f9286b242e2e8a13bbdc3c
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-06-08 23:25:02 -0400

    Lease out large

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -2067,7 +2067,7 @@ export const dailyWordsLarge = [
     'LAXED', 'LAXER', 'LAXES', 'LAXLY', 'LAYED', 'LAYIN', 'LAYUP', 'LAZAR', 'LAZED',
     'LAZES', 'LAZOS', 'LAZZI', 'LAZZO', 'LEACH', 'LEADS', 'LEADY', 'LEAFS', 'LEAKS',
     'LEAKY', 'LEAMS', 'LEANS', 'LEANT', 'LEANY', 'LEAPS', 'LEARE', 'LEARS',
-    'LEARY', 'LEASE', 'LEAST', 'LEATS', 'LEAVY', 'LEAZE', 'LEBEN', 'LECCY',
+    'LEARY', 'LEAST', 'LEATS', 'LEAVY', 'LEAZE', 'LEBEN', 'LECCY',
     'LEDES', 'LEDGY', 'LEDUM', 'LEEAR', 'LEEKS', 'LEEPS', 'LEERS',
     'LEESE', 'LEETS', 'LEEZE', 'LEFTE', 'LEFTS', 'LEFTY', 'LEGAL', 'LEGER', 'LEGES', 'LEGGE',
     'LEGGO', 'LEGIT', 'LEHRS', 'LEHUA', 'LEIRS', 'LEISH', 'LEMAN', 'LEMED', 'LEMEL',
