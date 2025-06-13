18f20e777e922186c95535b7901c75d9299ba3ae
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-06-13 02:18:00 -0400

    Vixen plus bs...like all em an span blind attempt

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function handleWordInput() {
           and ye had ye a score of '${foundWord.myScore}'!<br> Your Score Average thru this here stupid ass date: ${averageScoreUpToDate}!<br> Do NOT guess '${foundWord.word}', and do not waste our time!`;
     document.querySelector(".field-one").innerHTML = resultString;
   } else {
-    const notFoundString = `The word "${inputWord}", which is an innocent bystander, was notteth found, wretched Whores and Assfucks, making Rand Paul that rapist. Hit that fuck again neighbor. Shut up, but<br> feel free, feel obligated, to guess the word "${inputWord}", Fuckface! You're <em>all</em> fuckfaces.`;
+    const notFoundString = `The word "${inputWord}", which is an innocent bystander, was notteth found, wretched Whores and Assfucks, making Rand Paul that rapist. Hit that fuck again neighbor. Shut up, but<br> feel free, feel obligated, to guess the word "${inputWord}", Fuckface! You're <span><em>all</em></span> fuckfaces.`;
     document.querySelector(".field-one").innerHTML = notFoundString;
   }
 }
