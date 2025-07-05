15d3d361c99ab42a0a975bac3ad999f6acff2bed
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-07-04 23:36:32 -0400

    Curve plus bs

--- clockStuff.js
+++ clockStuff.js
@@ -64,7 +64,7 @@ function calculateCountdown(targetDate) {
   const minutes = Math.floor((diff / (1000 * 60)) % 60);
   const seconds = Math.floor((diff / 1000) % 60);
 
-  return `${days} very dark, long flick ass days, you extremely dumb fuck bitch fuckass drunks, ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  return `${days} very dark, long flick ass days, you know, you extremely dumb fuck bitch fuckass drunks, ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
 function updateCountdown() {
