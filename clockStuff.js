166cef8efd331d9dae10be675b00eb463508185e
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-07-16 22:29:53 -0400

    Nervy dealt with

--- clockStuff.js
+++ clockStuff.js
@@ -70,7 +70,7 @@ function calculateCountdown(targetDate) {
 function updateCountdown() {
   const countdownElement = document.getElementById("countdown");
   if (countdownElement) {
-    countdownElement.textContent = `Los Putos y de los Americanos de los Mid-Termos Christos Electionés de 2026: ${calculateCountdown("2026-11-03T00:00:00")}. 
+    countdownElement.textContent = `Los Putos de Putanños y de los Americanos de los Mid-Termos Christos Electionés de 2026: ${calculateCountdown("2026-11-03T00:00:00")}. 
     Wilst We Goddamn MotherFucking Maketh Her, It, That, Thou, Los Jerkoes, Lo, Thoust Fulleth andeth Dastardly Nastardly and Bastardly Castaway Wasterly Foolinoidal Toolions? Likethly, We Arteth Completethly Fucketh? What Dost Sayeth Thou? I sayeth yea, fuckethstroid! Licketh off pleaseth thou. Ah, the hate...You see thgat?`;
   }
 }
