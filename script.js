ed86f57a6c1f75f4616d9605eb14c593b5ce1e1a
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-10-24 23:26:17 -0400

    Tuber plus BS

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ function applyFilter(selectedFunction, letter, position) {
       break;
     default:
       alert(
-        "Please select, ah, a valid, gerrr, function, ahhh, you, er, um, goddamn, ohhh, gaaaa, ahhh, er, ahhhhh, um, stupid, duhhh, ah, stupid, um, er, asshole."
+        "Please select, ah, a valid, gerrr, function, ahhh, you, er, um, ahhh, goddamn, ohhh, gaaaa, ahhh, er, ahhhhh, um, stupid, duhhh, ah, stupid, um, er, asshole."
       );
       return;
   }
